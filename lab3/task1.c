efd8b8431e6f1e6d57eceffc9e1e94fef3642904
GoDL1ghT <sokolik23082005@gmail.com>
2025-05-28 23:33:02 +0500

    completed 1st and 4th tasks

--- lab3/task1.c
+++ lab3/task1.c
@@ -1,1 +1,61 @@
-// TODO
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <mpi.h>
+
+#define SIZE 1000000
+
+int main(int argc, char *argv[]) {
+    int *array = NULL;
+    long long seq_sum = 0;
+    double start_time, end_time;
+
+    array = malloc(SIZE * sizeof(int));
+    srand(time(NULL));
+    for (int i = 0; i < SIZE; i++) {
+        array[i] = rand() % 100;
+    }
+
+    start_time = MPI_Wtime();
+    for (int i = 0; i < SIZE; i++) {
+        seq_sum += array[i];
+    }
+    end_time = MPI_Wtime();
+
+    printf("Sequential total sum: %lld\n", seq_sum);
+    printf("Sequential time: %.6f seconds\n", end_time - start_time);
+
+    int rank, size;
+    int *sub_array = NULL;
+    long long local_sum = 0, total_sum = 0;
+
+    MPI_Init(&argc, &argv);
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+    MPI_Comm_size(MPI_COMM_WORLD, &size);
+
+    int chunk_size = SIZE / size;
+    sub_array = malloc(chunk_size * sizeof(int));
+
+    // Замер времени только на корневом процессе
+    if (rank == 0)
+        start_time = MPI_Wtime();
+
+    MPI_Scatter(array, chunk_size, MPI_INT, sub_array, chunk_size, MPI_INT, 0, MPI_COMM_WORLD);
+
+    for (int i = 0; i < chunk_size; i++) {
+        local_sum += sub_array[i];
+    }
+
+    MPI_Reduce(&local_sum, &total_sum, 1, MPI_LONG_LONG, MPI_SUM, 0, MPI_COMM_WORLD);
+
+    if (rank == 0) {
+        end_time = MPI_Wtime();
+        printf("Parallel total sum: %lld\n", total_sum);
+        printf("Parallel time: %.6f seconds\n", end_time - start_time);
+    }
+
+    free(array);
+    free(sub_array);
+    MPI_Finalize();
+    return 0;
+}
--- lab3/task4.c
+++ lab3/task4.c
@@ -1,1 +1,120 @@
-// TODO
+#include <mpi.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+#define ROWS 250
+#define COLS 400  // 250 * 400 = 100000
+
+void fill_matrix(double* matrix, int rows, int cols) {
+    for (int i = 0; i < rows * cols; i++) {
+        matrix[i] = (double)(rand() % 100 + 1);
+    }
+}
+
+int main(int argc, char** argv) {
+    int rank, size;
+    MPI_Init(&argc, &argv);
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+    MPI_Comm_size(MPI_COMM_WORLD, &size);
+
+    int total_elements = ROWS * COLS;
+    int chunk_size = total_elements / size;
+
+    double* A = NULL;
+    double* B = NULL;
+
+    if (rank == 0) {
+        A = (double*)malloc(total_elements * sizeof(double));
+        B = (double*)malloc(total_elements * sizeof(double));
+
+        srand(time(NULL));
+        fill_matrix(A, ROWS, COLS);
+        fill_matrix(B, ROWS, COLS);
+
+        // sequential version
+        double* seq_add = (double*)malloc(total_elements * sizeof(double));
+        double* seq_sub = (double*)malloc(total_elements * sizeof(double));
+        double* seq_mul = (double*)malloc(total_elements * sizeof(double));
+        double* seq_div = (double*)malloc(total_elements * sizeof(double));
+
+        double start_seq = MPI_Wtime();
+        for (int i = 0; i < total_elements; i++) {
+            seq_add[i] = A[i] + B[i];
+            seq_sub[i] = A[i] - B[i];
+            seq_mul[i] = A[i] * B[i];
+            seq_div[i] = A[i] / B[i];
+        }
+        double end_seq = MPI_Wtime();
+        printf("Sequential time: %f seconds\n", end_seq - start_seq);
+
+        free(seq_add);
+        free(seq_sub);
+        free(seq_mul);
+        free(seq_div);
+    }
+
+    // parallel version
+    double* result_add = (double*)malloc(chunk_size * sizeof(double));
+    double* result_sub = (double*)malloc(chunk_size * sizeof(double));
+    double* result_mul = (double*)malloc(chunk_size * sizeof(double));
+    double* result_div = (double*)malloc(chunk_size * sizeof(double));
+
+    double* local_A = (double*)malloc(chunk_size * sizeof(double));
+    double* local_B = (double*)malloc(chunk_size * sizeof(double));
+
+    MPI_Scatter(A, chunk_size, MPI_DOUBLE, local_A, chunk_size, MPI_DOUBLE, 0, MPI_COMM_WORLD);
+    MPI_Scatter(B, chunk_size, MPI_DOUBLE, local_B, chunk_size, MPI_DOUBLE, 0, MPI_COMM_WORLD);
+
+    MPI_Barrier(MPI_COMM_WORLD);
+    double start_parallel = MPI_Wtime();
+
+    for (int i = 0; i < chunk_size; i++) {
+        result_add[i] = local_A[i] + local_B[i];
+        result_sub[i] = local_A[i] - local_B[i];
+        result_mul[i] = local_A[i] * local_B[i];
+        result_div[i] = local_A[i] / local_B[i];
+    }
+
+    MPI_Barrier(MPI_COMM_WORLD);
+    double end_parallel = MPI_Wtime();
+
+    if (rank == 0) {
+        double* full_add = (double*)malloc(total_elements * sizeof(double));
+        double* full_sub = (double*)malloc(total_elements * sizeof(double));
+        double* full_mul = (double*)malloc(total_elements * sizeof(double));
+        double* full_div = (double*)malloc(total_elements * sizeof(double));
+
+        MPI_Gather(result_add, chunk_size, MPI_DOUBLE, full_add, chunk_size, MPI_DOUBLE, 0, MPI_COMM_WORLD);
+        MPI_Gather(result_sub, chunk_size, MPI_DOUBLE, full_sub, chunk_size, MPI_DOUBLE, 0, MPI_COMM_WORLD);
+        MPI_Gather(result_mul, chunk_size, MPI_DOUBLE, full_mul, chunk_size, MPI_DOUBLE, 0, MPI_COMM_WORLD);
+        MPI_Gather(result_div, chunk_size, MPI_DOUBLE, full_div, chunk_size, MPI_DOUBLE, 0, MPI_COMM_WORLD);
+
+        printf("Parallel time: %f seconds\n", end_parallel - start_parallel);
+
+        free(full_add);
+        free(full_sub);
+        free(full_mul);
+        free(full_div);
+    } else {
+        MPI_Gather(result_add, chunk_size, MPI_DOUBLE, NULL, chunk_size, MPI_DOUBLE, 0, MPI_COMM_WORLD);
+        MPI_Gather(result_sub, chunk_size, MPI_DOUBLE, NULL, chunk_size, MPI_DOUBLE, 0, MPI_COMM_WORLD);
+        MPI_Gather(result_mul, chunk_size, MPI_DOUBLE, NULL, chunk_size, MPI_DOUBLE, 0, MPI_COMM_WORLD);
+        MPI_Gather(result_div, chunk_size, MPI_DOUBLE, NULL, chunk_size, MPI_DOUBLE, 0, MPI_COMM_WORLD);
+    }
+
+    free(local_A);
+    free(local_B);
+    free(result_add);
+    free(result_sub);
+    free(result_mul);
+    free(result_div);
+
+    if (rank == 0) {
+        free(A);
+        free(B);
+    }
+
+    MPI_Finalize();
+    return 0;
+}
